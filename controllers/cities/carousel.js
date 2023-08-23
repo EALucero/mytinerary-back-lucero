9a936759a7e687bc55d3da5008b8ff12f6676032
EALucero <ea.lucero@outlook.com>
2023-08-23 09:05:42 -0300

    fix

--- controllers/cities/carousel.js
+++ controllers/cities/carousel.js
@@ -2,7 +2,7 @@ import City from "../../models/City.js";
 
 export default async (req, res, next) => {
     try {
-        let all = await City.find({}, 'city photo').sort({ fundation: -1 }).limit(12)
+        let all = await City.find({}, 'city photo').sort({ fundation: -1 }).limit(16)
         //let all = await City.find({}, 'photo city').sort({ fundation: 'desc' })
         let count = await City.countDocuments()  //if (count / 4 % 3) {}
         return res.status(200).json({
