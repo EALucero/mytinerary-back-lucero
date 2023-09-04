094113837141d6efdeb1a335dfc75c1d656abcda
EALucero <56940163+EALucero@users.noreply.github.com>
2023-09-04 03:48:53 -0300

    s3 (#1)

--- controllers/cities/readOne.js
+++ controllers/cities/readOne.js
@@ -2,7 +2,12 @@ import City from "../../models/City.js";
 
 export default async (req, res, next) => {
     try {
-        let oneCity = await City.findOne({ _id: req.params.id }).select("city photo country smallDescription -_id")
+        let oneCity = await City.findOne({ _id: req.params.id }).select("city photo country smallDescription admin_id -_id")
+        .populate({
+            path: "admin_id",
+            select: "name photo _id",
+        });
+
         if (oneCity) {
             return res.status(200).json({
                 success: true,
